6bb7967e92b8fe7d09f74d25170c10bcbb8735df
isaiah <isotodev@gmail.com>
2021-01-04 11:08:10 -0600

    Check if palindrome functionallity added

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace challenge_palindrome_Saixah
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(isPalindrome())
+                Console.WriteLine("Is a Palindrom");
+            else 
+                Console.WriteLine("Not a Palindrome");
+        }
+
+        public static bool isPalindrome()
+        {
+            Console.WriteLine("Please Enter a Word");
+            var UserInput = Console.ReadLine();
+            var Reversed = new string(UserInput.Reverse().ToArray());
+            var isPalindrom = UserInput == Reversed;
+            return isPalindrom;
+        }
+    }
+}
